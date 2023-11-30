6454962d999421167730f0c4a0917c58cf332a9b
Laadnani mohamed <laadnani.med@gmail.com>
2023-11-30 10:51:39 -0800

    local mess

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,9 +3,9 @@
 if __name__ == "__main__":
     import hidden_4
 
-    list = dir(hidden_4)
-    for i in list:
-        if name[:2] == "__":
+    list_name = dir(hidden_4)
+    for i in list_name:
+        if i[:2] == "__":
             continue
         else:
             print(i)
