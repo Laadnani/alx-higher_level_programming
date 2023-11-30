84f70d8f9fe10064eeb9a9fde2f5bf8c90a58cbf
Laadnani mohamed <laadnani.med@gmail.com>
2023-11-30 10:40:14 -0800

    llc

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    value = 0
+    for i in range(1, len(sys.argv)):
+        value += int(sys.argv[i])
+    print("{}" .format(value))
