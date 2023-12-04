9817f53d9938eb9e2eee074b7cbdf9a1aab9cac1
Laadnani mohamed <laadnani.med@gmail.com>
2023-12-04 06:04:07 -0800

    adding nothing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     for a in matrix:
+        if b in a == 0:
+            print()
         for b in a:
             print("{:d}" .format(b), end=" " if b != a[-1] else"")
         print()
