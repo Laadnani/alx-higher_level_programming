af054479eca88837020a7399a1ec4d2f4439885c
Laadnani mohamed <laadnani.med@gmail.com>
2023-12-04 07:07:55 -0800

    allucard

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,3 @@
 #!/usr/bin/phyton3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return None
-    else:
-        a = len(sentence)
-        b = sentence[0]
-        return (a, b)
+    return (len(sentence, sentence[0] if len(sentence) > 0 else return None)
