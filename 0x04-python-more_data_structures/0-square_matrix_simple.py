e9b4fbc3f6f30462987654a3178292a0e4ee364f
Laadnani mohamed <laadnani.med@gmail.com>
2023-12-06 06:57:01 -0800

    all

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,9 @@
 def square_matrix_simple(matrix=[]):
     quad = lambda z : z ** 2
     new = []
-    for i in matrix:
-        new.append(list(map(quad, i)))
-    return new
+    if  not  matrix:
+        return new
+    else:
+        for i in matrix:
+            new.append(list(map(quad, i)))
+        return new
