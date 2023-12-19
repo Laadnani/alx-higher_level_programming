ecf56ff07651bd0a6310ffae867bab24715bfb38
Laadnani mohamed <laadnani.med@gmail.com>
2023-12-19 10:42:10 -0800

    asa

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-""" Define a Class """
-
+"""Define a Class"""
 
 class Square:
     """ Define a squre"""
+
     def __init__(self, size=0):
-    """Constructor.
 
-    Args:
-        size: side size of the square.
-
-    Raise:
-        TypeError: if the size is not an int
-        ValueError: if size if less than 0
-    """
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define Square"""
+
+class Square:
+    """ Define a sqare """
+
+    def __init__(self, size=0):
+
