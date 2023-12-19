201ee3a43f9bbd142490c5b50606cbd1c6973542
Laadnani mohamed <laadnani.med@gmail.com>
2023-12-19 10:25:56 -0800

    pycod

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+"""Square Class"""
+
+class Square:
+    """ Defines a Square """
+    def __init__(self, size):
+        """ Constructor.
+
+        Args:
+            Size: size of one side of the squre
+        """
+        self.__size = size
