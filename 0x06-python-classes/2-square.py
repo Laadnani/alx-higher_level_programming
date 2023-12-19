d1724ef579ba140038a32cc311ff27c1007d1c5b
Laadnani mohamed <laadnani.med@gmail.com>
2023-12-19 10:34:39 -0800

    maybe

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!usr/bin/python3
+""" Define a Class """
+
+
+class Square:
+    """Constructor.
+
+    Args:
+        size: side size of the square.
+
+    Raise:
+        TypeError: if the size is not an int
+        ValueError: if size if less than 0
+    """
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('Size must be >= 0')
+    self.__size = size
