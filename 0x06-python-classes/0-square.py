522df029d0845aee922d744be53208b92ba80fa8
Laadnani mohamed <laadnani.med@gmail.com>
2023-12-19 10:20:03 -0800

    allo

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-''' define a class '''
+''' Square class '''
 
 class Square:
-
-    def __init__(self):
-        " Define a squre "
-        pass
+    " Define a Square "
+    pass
