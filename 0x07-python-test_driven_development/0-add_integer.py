abba2f7c96681db5105b0d872f8300c8c128878a
Laadnani mohamed <laadnani.med@gmail.com>
2024-01-07 05:15:13 -0800

    allow it

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a module used to add to intergers."""
+
+def add_integer(a, b=98):
+    """
+    adding a + b
+    a and b must be int and/or floats
+    the func return returs the addition of a and b
+    """
+
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    elif type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
