c7412b068f28d7d93c0cf395a9ee08d5516cec87
Laadnani mohamed <laadnani.med@gmail.com>
2024-01-10 10:24:46 -0800

    allow case

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """read_file function that read a file"""
 
-def write_file(filename="", text=""):
+def read_file(filename=""):
     """ reads filename with utf-8 encoding"""
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
