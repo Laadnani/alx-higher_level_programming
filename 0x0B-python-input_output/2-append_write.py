57085c1922872858141e59b744d3e5b3f99ef6c7
Laadnani mohamed <laadnani.med@gmail.com>
2024-01-10 10:41:06 -0800

    uppercase

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,10 @@
-#!/usr/bin/pyrhon3
-"""def of the entire mod"""
+#!/usr/bin/python3
+"""
+Contains the function "append_wrtie"
+"""
+
 
 def append_write(filename="", text=""):
-    """appending filename with utf-8 enc"""
-    with open(filename, 'a', encoding="utf-8") as f:
+    """returns the number of chars appended to "filename" from "text" """
+    with open(filename, 'a', encoding='utf=8') as f:
         return f.write(text)
