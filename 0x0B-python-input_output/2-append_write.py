0e84b86ab51f42744594455e1f2e9a254674197c
Laadnani mohamed <laadnani.med@gmail.com>
2024-01-10 10:34:25 -0800

    alloc

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,5 +3,5 @@
 
 def append_write(filename="", text=""):
     """appending filename with utf-8 enc"""
-    with open(filename, 'a', encoding='utf-8') as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
