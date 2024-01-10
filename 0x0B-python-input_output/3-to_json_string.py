c749ba740f78a6c7b7430679c20342d3222be020
Laadnani mohamed <laadnani.med@gmail.com>
2024-01-10 10:32:55 -0800

    json adds

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" def of the mod"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returning strings from JSON obj"""
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" def of the mod"""
+
+import json
+
+
+def from_json_string(my_str):
+    """returning JSON string"""
+    return json.loads(my_str)
